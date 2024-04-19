482bc444729a1d9e53280b7e04ed88c501a54b04
Ranendra Das <rndas@dtcc.com>
2024-04-19 15:50:37 -0400

    Initial commit

---
+++ src/main/java/com/test/api/controller/JenkinTest.java
@@ -0,0 +1,18 @@
+package com.test.api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@Slf4j
+public class JenkinTest {
+
+    @GetMapping
+    public String test(){
+        log.info("Jenkin test successful");
+        return "Jenkin test successful";
+    }
+}
